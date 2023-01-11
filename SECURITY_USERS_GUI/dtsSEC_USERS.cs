d4eebb5831696f6a3dc24f0390e123199faf0b61
Victor Zhukov <v.zhukov@simplex48.ru>
2023-01-11 16:37:26 +0300

    Начинаю работы с SEC_USERS

---
+++ SECURITY_USERS_GUI/dtsSEC_USERS.cs
@@ -0,0 +1,15 @@
+﻿namespace SECURITY_USERS_GUI
+{
+
+
+    partial class dtsSEC_USERS
+    {
+    }
+}
+
+namespace SECURITY_USERS_GUI.dtsSEC_USERSTableAdapters {
+    
+    
+    public partial class SEC_USERTableAdapter {
+    }
+}
